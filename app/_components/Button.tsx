18da4acf0f4ed8edfb309678d5e1f8c37a615f28
Mahred1 <mredwan.kemal@gmail.com>
2026-07-15 17:03:24 +0300

    finished the button component

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -5,19 +5,30 @@ import Image from "next/image";
 type Props = {
   children: string;
   type: "primary" | "secondary";
-  icon: boolean;
+  src?: string;
   alt: string;
 };
 
-const primary =
-  "bg-foreground font-inter font-medium text-md w-fit px-4 py-2 border-foreground border-3 text-secondary flex gap-2";
-const secondary =
-  "bg-background font-inter text-lg w-fit px-4 py-2.5 border-foreground border-3 text-primary flex gap-2";
+const baseStyle = "font-inter text-md w-fit px-2 py-1.5 flex gap-2  ";
 
-const Button = ({ children, type, icon, alt }: Props) => {
+const variants = {
+  primary:
+    "bg-foreground  font-medium  border-foreground border-2 text-secondary  ",
+  secondary: "bg-background  border-foreground border-2 text-primary",
+};
+
+const Button = ({ children, type, src, alt }: Props) => {
   return (
-    <button className={cn(type === "primary" ? primary : secondary)}>
-      {icon && <Image src="/file.svg" width={16} height={16} alt={alt}></Image>}
+    <button className={cn(baseStyle, variants[type])}>
+      {src && (
+        <Image
+          className={cn(type === "primary" ? "text-secondary" : "text-primary")}
+          src={src}
+          width={16}
+          height={16}
+          alt={alt}
+        ></Image>
+      )}
       <span>{children}</span>
     </button>
   );
