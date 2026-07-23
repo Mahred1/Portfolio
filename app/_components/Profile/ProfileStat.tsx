cd7dc52731f6867b06870d1b0bf59540add82e75
Mahred1 <mredwan.kemal@gmail.com>
2026-07-23 11:26:21 +0300

    integrated sanity

--- app/_components/Profile/ProfileStat.tsx
+++ app/_components/Profile/ProfileStat.tsx
@@ -4,8 +4,8 @@ import React from "react";
 const ProfileStat = ({ value, title, className }: { value: string; title: string; className: string }) => {
   return (
     <div className={cn("border-foreground flex w-full flex-col items-center gap-0 border-2 py-1", className)}>
-      <h2 className="text-lg font-mono font-bold">{value}</h2>
-      <span className="font-mono text-gray-500 text-base ">{title}</span>
+      <h2 className="text-xl font-mono font-bold">{value}</h2>
+      <span className="font-mono text-gray-500 text-sm ">{title}</span>
     </div>
   );
 };
